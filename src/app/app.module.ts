8221f2cffb9c99f4bb8fd59f5a9abe95bbef89a7
avoronkoff <voronkoff.aleksei@gmail.com>
2017-07-25 18:45:00 +0300

    add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TabsModule } from 'ngx-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './main/app.component';
+import { AppNavbarComponent } from './navbar/app.navbar.component';
+import { AppNavpillsComponent } from './navpills/app.navpills.component';
+import { AppTabsComponent } from './tabs/app.tabs.component';
+
+import { AppGridService } from './service/app.grid.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent,
+    AppNavpillsComponent,
+    AppTabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    TabsModule.forRoot()
   ],
-  providers: [],
+  providers: [AppGridService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
